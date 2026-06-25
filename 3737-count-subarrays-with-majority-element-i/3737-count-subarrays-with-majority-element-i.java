83f2d03a2ca941739be64e4d0608fdd556e94881
Krishna-Tayal <161112930+Krishna-Tayal@users.noreply.github.com>
2026-06-25 23:47:19 +0530

    Time: 27 ms (80%), Space: 47 MB (40.92%) - LeetHub

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.java
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.java
@@ -20,5 +20,27 @@ class Solution {
         }
 
         return ans;
+
+
+    // int n=nums.length;
+    //     int sub=0;
+    //     for(int i=0;i<n;i++){
+    //         for(int j=i;j<n;j++){
+    //             sub+=majority(nums,i,j,target);
+    //         }
+    //     }
+    //     return sub;
+    // public int majority(int[] nums, int i,int j,int target){
+    //     int len=j-i+1;
+    //     int count=0;
+    //     for(int k=i;k<=j;k++){
+    //         if(nums[k]==target){
+    //             count++;
+    //         }
+    //     }
+    //     if(count > len/2){
+    //         return 1;
+    //     }
+    //     return 0;
     }
 }
